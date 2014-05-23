5aa6eb1597215c006a18cecd9c3e7429d93413ee
Jacob Zweifel <jakez834@gmail.com>
2014-05-23 01:36:56 -0500

    initial
    Wrote up data model classes, spins up a local database. Doesn't yet
    successfully initialize a db with data, and the console app has yet to
    sucessfully read from the log file.

---
+++ SeedDatabase/Program.cs
@@ -0,0 +1,53 @@
+﻿using HearthstoneLogData.DAL;
+using HearthstoneLogData.Models;
+using HearthstoneLogReader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeedDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Open up our database
+            HearthstoneLogDataContext context = new HearthstoneLogDataContext();
+
+            Console.WriteLine("DB initialized and open.");
+            Console.WriteLine("");
+
+            //var match = new Match { MatchStart = DateTime.Now, MatchEnd = DateTime.Now };
+
+            //context.Matches.Add(match);
+            //context.SaveChanges();
+
+            //Console.WriteLine("New match added.");
+            //Console.WriteLine("");
+
+            //Console.WriteLine("All matches in the db:");
+
+            //var query = from b in context.Matches
+            //                orderby b.MatchStart
+            //                select b;
+
+            //foreach (var item in query)
+            //{
+            //    Console.WriteLine(String.Format("Start Time: {0} End Time: {1}", item.MatchStart, item.MatchEnd));
+            //}
+
+            LogReader lr = new LogReader();
+            lr.FilePath = @"C:\Users\jakez834\Documents\GitHub\HearthstoneLogReader\HearthstoneLogReader\sample_data\output_log.txt";
+
+            lr.StartReadLog();
+
+            Console.WriteLine("");
+            Console.WriteLine("Press enter to exit...");
+
+            Console.ReadLine();
+        }
+    }
+}
